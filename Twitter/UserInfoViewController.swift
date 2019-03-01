2bc6a60c49130cfc95c87048c2416fc6f25c4a4e
Kimleng <kimleng@github.com>
2019-02-28 23:50:54 -0800

    Update the UserInfoVC

--- Twitter/UserInfoViewController.swift
+++ Twitter/UserInfoViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AlamofireImage
 
 class UserInfoViewController: UIViewController {
 
@@ -29,10 +30,12 @@ class UserInfoViewController: UIViewController {
             let followersCount = user["followers_count"] as! Int
             let tweets = user["listed_count"] as! Int
             let following = user["friends_count"] as! Int
+            let imageString = user["profile_image_url"] as! String
+            self.imageView.af_setImage(withURL: URL(string: imageString)!)
             self.tagLine.text = taglineText
-            self.followers.text = String(followersCount)
-            self.tweets.text = String(tweets)
-            self.following.text = String(following)
+            self.followers.text = String("Followers: \(followersCount)")
+            self.tweets.text = String("#Tweets: \(tweets)")
+            self.following.text = String("Following: \(following)")
         }
     }
     
